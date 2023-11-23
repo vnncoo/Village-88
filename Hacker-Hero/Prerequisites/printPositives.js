366a965fc7165af5629d1fdd697d91b24e4775a0
Ivann Comiso <vnncoo@gmail.com>
2023-11-24 05:58:53 +0700

    upl.hh-pr/s

---
+++ Hacker-Hero/Prerequisites/printPositives.js
@@ -0,0 +1,11 @@
+function printPositives(arr){
+	// Enter code below
+	let iterations = arr.length;
+
+	for (let i = 0; i < iterations; i++){
+		if (arr[i] > 0){
+        		console.log(arr[i]);
+		}
+	}
+
+}
