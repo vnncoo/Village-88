c8efe68ce3da11d2566530fbac56cfb7ff7ba7b8
Ivann Comiso <vnncoo@gmail.com>
2023-11-24 05:57:11 +0700

    upl.hh-pr/s

---
+++ Hacker-Hero/Prerequisites/noNeg.js
@@ -0,0 +1,14 @@
+function noNeg(arr){
+	// Enter code below
+	let length = arr.length;
+
+	for (let i = 0; i < length; i++){
+
+		if (arr[i] < 1){
+
+		        arr[i] = 0;
+
+		}
+	}
+	return arr;
+}
