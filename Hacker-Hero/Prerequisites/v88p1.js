f7a53d9d82d4b91935c0e48af4dc26333bea7f5a
Ivann Comiso <vnncoo@gmail.com>
2023-11-23 23:58:13 +0700

    upl.hh.p1-5
    Prerequisites of Village 88 from 1-5

---
+++ Hacker-Hero/Prerequisites/v88p1.js
@@ -0,0 +1,8 @@
+function print1to20(){
+	for (let i = 0; i < 20; i ++) {
+		    console.log(i + 1);
+
+	}
+}
+
+
---
+++ Hacker-Hero/Prerequisites/v88p5.js
@@ -0,0 +1,7 @@
+function countdownByFives(){
+	let from = 50
+	while (from != 0){
+		console.log(from);
+		from = from - 50;
+	}
+}
