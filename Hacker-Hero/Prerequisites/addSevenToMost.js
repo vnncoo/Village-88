dde9c582ebc68a0086e8b42b25a75cf5331808f1
Ivann Comiso <vnncoo@gmail.com>
2023-11-24 07:42:30 +0700

    upd.v88/hh/p-astm

--- Hacker-Hero/Prerequisites/addSevenToMost.js
+++ Hacker-Hero/Prerequisites/addSevenToMost.js
@@ -1,5 +1,13 @@
-function firstPlusLength(arr){
+function addSevenToMost(arr){	
 	// Enter code below
-	let toPrint = arr[0] + arr.length;
-	console.log(tpPrint);
+	let length = arr.length;
+	let newArr = [];
+
+	for(i = 0; i < length - 1; i++){
+		console.log[arr];
+	    	for(j = 0; j < length-1; j++){
+			newArr[i] = (arr[i+1]+7);
+    		}
+	}
+	return newArr;
 }
