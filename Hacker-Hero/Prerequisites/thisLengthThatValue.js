5df8a4de20a39914fa45cae9844b8dfc863a421a
Ivann Comiso <vnncoo@gmail.com>
2023-11-24 07:17:43 +0700

    fix.v88/hh/p-tltv

--- Hacker-Hero/Prerequisites/thisLengthThatValue.js
+++ Hacker-Hero/Prerequisites/thisLengthThatValue.js
@@ -1,5 +1,11 @@
-function firstPlusLength(arr){
+function thisLengthThatValue(num1, num2){
 	// Enter code below
-	let toPrint = arr[0] + arr.length;
-	console.log(tpPrint);
+
+	let number = num2;
+	let arr = [];
+
+	for (i = 0; i < num1; i++){
+		arr[i] = number;
+	}
+	return arr;
 }
