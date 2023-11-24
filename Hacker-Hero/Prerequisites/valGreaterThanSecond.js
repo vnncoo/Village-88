cdcfd2ffd9fee064e317f1e3e4a7a2212420826a
Ivann Comiso <vnncoo@gmail.com>
2023-11-24 07:09:24 +0700

    upd.v88/hh/p.vgts

--- Hacker-Hero/Prerequisites/valGreaterThanSecond.js
+++ Hacker-Hero/Prerequisites/valGreaterThanSecond.js
@@ -1,5 +1,16 @@
 function firstPlusLength(arr){
 	// Enter code below
-	let toPrint = arr[0] + arr.length;
-	console.log(tpPrint);
+	let length = arr.length;
+	
+	let sum = 0;
+	
+	for (i = 0; i < length; i++){
+		// secVal/second value is index of 1
+		let secVal = arr[1];
+		if (arr[i] > secVal){
+			console.log(arr[i]);
+	        	sum += arr[i];
+		}
+	}   
+	return sum;
 }
