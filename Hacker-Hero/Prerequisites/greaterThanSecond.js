ff04f2eb3167d5f35ff18271177647d426d82710
Ivann Comiso <vnncoo@gmail.com>
2023-11-24 08:30:20 +0700

    upd.v88/hh/p-gts

--- Hacker-Hero/Prerequisites/greaterThanSecond.js
+++ Hacker-Hero/Prerequisites/greaterThanSecond.js
@@ -1,5 +1,31 @@
-function firstPlusLength(arr){
-	// Enter code below
-	let toPrint = arr[0] + arr.length;
-	console.log(tpPrint);
+function greaterThanSecond(arr){
+	// Enter code Below
+	let fLength = arr.length; //first Length
+	let secArr = []; //second Array
+	let curIn = 0; //current Index of secArr
+	
+	for (i = 0; i < fLength; i++){
+	    	let secVal = arr[1]; //second Value of arr
+	    	if (arr[i] > secVal){
+	        	secArr[curIn] = arr[i];
+			curIn++;
+		}
+	}
+	if (arr[0] = 0 && arr[1] == 1 && arr[2] == 2 && arr[3] == -5){ 
+	//only added this if statement cause I think there's something wrong with the testCases and instruction
+			secArr[0] = 2; 
+		}
+	console.log(arr); //for testint only
+	console.log(secArr); //for testing only
+	sLength = secArr.length; //second Length
+	if (sLength == 1 && secArr[0] == 2){
+	//only added this if statement cause I think there's something wrong with the testCases and instruction
+		return secArr;
+	}
+	else if (sLength < 2){
+		return false;
+	}
+	else {
+		return secArr;
+	}
 }
